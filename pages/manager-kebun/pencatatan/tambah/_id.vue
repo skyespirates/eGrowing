3ff7be482808eac8701efcb99270c8b7822bc17d
Skyes Crawford <akashiseijurou414@gmail.com>
2022-08-02 08:39:29 +0700

    memperbaiki dan menambah pencatatan

--- pages/manager-kebun/pencatatan/tambah/_id.vue
+++ pages/manager-kebun/pencatatan/tambah/_id.vue
@@ -70,81 +70,115 @@
         <v-col cols="12" v-if="charian.tahapan_id" class="px-0">
           <v-card-text>
             <v-row>
-              <v-col>
-                <v-divider></v-divider>
-              </v-col>
-              <v-col cols="12">
-                Anda akan membuat
-                <strong
-                  >Laporan
-                  {{
-                    charian.tahapan_id == 1
-                      ? "Persiapan Lahan dan Tanaman"
-                      : charian.tahapan_id == 2
-                      ? "Perawatan Tanaman Belum Menghasilkan"
-                      : charian.tahapan_id == 3
-                      ? "Perawatan Tanaman Menghasilkan"
-                      : "Pemanenan"
-                  }}
-                </strong>
-                untuk <strong>Blok {{ charian.blok_lahan_id }}</strong> pada
-                tanggal
-                <strong>
-                  {{ new Date(charian.tanggal).getDate() }}
-                  {{ new Date(charian.tanggal).getMonth() | monthFormat }}
-                  {{ new Date(charian.tanggal).getFullYear() }}</strong
-                >
-              </v-col>
-              <v-col cols="12" v-for="sop in charian.kegiatan" :key="sop.id">
-                <v-row>
-                  <v-col cols="12" md="9">
-                    <v-checkbox
-                      hide-details=""
-                      false-value="0"
-                      true-value="1"
-                      v-model="sop.filled"
-                      :label="sop.item_pekerjaan"
-                      class="py-1 my-1"
-                    ></v-checkbox>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    md="3"
-                    class="d-flex align-center mx-0 my-0 py-0"
-                  >
-                    Durasi Waktu: {{ sop.durasi_waktu }}
-                  </v-col>
-                  <v-col>
-                    <v-divider></v-divider>
-                  </v-col>
-                </v-row>
-                <v-row no-gutters class="my-2" v-if="sop.filled == 1">
-                  <v-col
-                    cols="12 mb-1"
-                    v-for="indikator in sop.indikator"
-                    :key="indikator.id"
-                  >
-                    <!-- {{indikator}} -->
-                    <v-text-field
-                      :label="indikator.nama_indikator"
-                      dense
-                      outlined
-                      v-model="indikator.catat_jawaban"
-                      hide-details=""
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-col>
-              <v-col cols="12">
-                <v-textarea
-                  outlined
-                  label="Catatan"
-                  rows="3"
-                  persistent-placeholder
-                  v-model="charian.catatan"
-                ></v-textarea>
+              <v-col cols="12" md="9">
+                <v-checkbox
+                  hide-details=""
+                  false-value="0"
+                  true-value="1"
+                  label="Panen?"
+                  class="py-1 my-1"
+                  v-model="charian.panen"
+                ></v-checkbox>
               </v-col>
             </v-row>
+            <v-row v-if="charian.panen == 1">
+              <v-col cols="12" md="8">
+                <v-text-field
+                  outlined
+                  dense
+                  label="Panen Aktual"
+                  v-model="charian.panen_aktual"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="8">
+                <v-text-field
+                  outlined
+                  dense
+                  label="Panen Grade A"
+                  v-model="charian.panen_gradeA"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="8">
+                <v-text-field
+                  outlined
+                  dense
+                  label="Panen Grade B"
+                  v-model="charian.panen_gradeB"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-col cols="12">
+              Anda akan membuat
+              <strong
+                >Laporan
+                {{
+                  charian.tahapan_id == 1
+                    ? "Persiapan Lahan dan Tanaman"
+                    : charian.tahapan_id == 2
+                    ? "Perawatan Tanaman Belum Menghasilkan"
+                    : charian.tahapan_id == 3
+                    ? "Perawatan Tanaman Menghasilkan"
+                    : "Pemanenan"
+                }}
+              </strong>
+              untuk <strong>Blok {{ charian.blok_lahan_id }}</strong> pada
+              tanggal
+              <strong>
+                {{ new Date(charian.tanggal).getDate() }}
+                {{ new Date(charian.tanggal).getMonth() | monthFormat }}
+                {{ new Date(charian.tanggal).getFullYear() }}</strong
+              >
+            </v-col>
+            <v-col cols="12" v-for="sop in charian.kegiatan" :key="sop.id">
+              <v-row>
+                <v-col cols="12" md="9">
+                  <v-checkbox
+                    hide-details=""
+                    false-value="0"
+                    true-value="1"
+                    v-model="sop.filled"
+                    :label="sop.item_pekerjaan"
+                    class="py-1 my-1"
+                  ></v-checkbox>
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="3"
+                  class="d-flex align-center mx-0 my-0 py-0"
+                >
+                  Durasi Waktu: {{ sop.durasi_waktu }}
+                </v-col>
+                <v-col>
+                  <v-divider></v-divider>
+                </v-col>
+              </v-row>
+              <v-row no-gutters class="my-2" v-if="sop.filled == 1">
+                <v-col
+                  cols="12 mb-1"
+                  v-for="indikator in sop.indikator"
+                  :key="indikator.id"
+                >
+                  <!-- {{indikator}} -->
+                  <v-text-field
+                    :label="indikator.nama_indikator"
+                    dense
+                    outlined
+                    v-model="indikator.catat_jawaban"
+                    hide-details=""
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="12">
+              <v-textarea
+                outlined
+                label="Catatan"
+                rows="3"
+                persistent-placeholder
+                v-model="charian.catatan"
+              ></v-textarea>
+            </v-col>
           </v-card-text>
           <v-card-actions>
             <v-btn
@@ -167,12 +201,17 @@ export default {
   // middleware: "permission",
   data() {
     return {
+      sop_id: "",
+      blok_lahan: "",
       charian: {
-        blok_lahan_id: "",
         tahapan_id: "",
+        panen: false,
+        panen_aktual: "",
+        panen_gradeA: "",
+        panen_gradeB: "",
         kegiatan: [],
         catatan: "",
-        tanggal: ""
+        tanggal: "",
       },
       disable: false,
       nama: "",
@@ -180,35 +219,43 @@ export default {
       tahapans: [],
       project: {
         sop: {
-          nama_sop: ""
+          nama_sop: "",
         },
         sop_id: "",
         alamat: {
-          alamat: {}
-        }
-      }
+          alamat: {},
+        },
+      },
     };
   },
   computed: {
     nama_sop() {
       this.getSop(this.project.sop_id);
       return this.nama;
-    }
+    },
   },
   mounted() {
     this.getProject();
-    this.getTahapan();
-    this.getSop(this.$route.params.id);
+    this.getBlok(this.$route.params.id);
   },
   methods: {
-    async getTahapan() {
+    async getBlok(id) {
       try {
-        let response = await this.$axios.get("api/v1/tahapan");
-        this.tahapans = response.data.tahapan;
-        console.log(response);
+        const res = this.$axios.get("api/v1/sop/" + id);
+        // console.log(res.data);
+      } catch (error) {
+        this.error = true;
+        console.log(error.message);
+      }
+    },
+    async getTahapan(sop_id) {
+      try {
+        const res = await this.$axios.get(`api/v1/sop/${sop_id}/tahapan`);
+        console.log();
+        this.tahapans = res.data.data;
       } catch (err) {
         this.error = true;
-        console.log(err);
+        console.log(err.message);
       }
     },
     async getProject() {
@@ -216,8 +263,10 @@ export default {
         let response = await this.$axios.get(
           "api/v1/project/" + this.$route.params.id
         );
+        this.sop_id = response.data.data.sop_id;
+        this.getTahapan(response.data.data.sop_id);
+        // console.log(res);
         this.project = response.data.data;
-        console.log(response);
       } catch (err) {
         this.error = true;
         console.log(err);
@@ -225,7 +274,7 @@ export default {
     },
     async getSop(id) {
       try {
-        let response = await this.$axios.get("/api/v1/sop/" + id);
+        let response = await this.$axios.get("/api/v1/sop/" + 24);
         this.nama = response.data.data.sop_nama;
         this.sops = response.data.data;
       } catch (err) {
@@ -233,44 +282,46 @@ export default {
         console.log(err);
       }
     },
-    isiCatatan() {
+    async isiCatatan() {
       this.disable = true;
+
       let f = this.sops.item_pekerjaan
-        .filter(e => e.tahapan_sop_id == this.charian.tahapan_id)
-        .forEach(element => {
+        .filter((e) => e.tahapan_sop_id == this.charian.tahapan_id)
+        .forEach((element) => {
           this.charian.kegiatan.push({
+            item_pekerjaan_id: element.id,
             item_pekerjaan: element.nama_kegiatan,
-            durasi_waktu: element.durasi_waktu,
             filled: 0,
-            indikator: []
+            indikator: [],
           });
-          // console.log(element);
-          // let g = this.charian.kegiatan
-          //   .filter(e => e.item_pekerjaan == element.nama_kegiatan)
-          //   .forEach(e1 => {
-          //     e1.indikator.push({
-          //       nama_indikator: e2.nama_indikator,
-          //       catat_jawaban: null
-          //     })
-          //   })
-          // console.log(g);
-          element.indikator_kegiatan.forEach(e2 => {
+          element.indikator_kegiatan.forEach((e2) => {
             let j = this.charian.kegiatan
-              .filter(e => e.item_pekerjaan == element.nama_kegiatan)
-              .forEach(e1 => {
+              .filter((e) => e.item_pekerjaan == element.nama_kegiatan)
+              .forEach((e1) => {
                 e1.indikator.push({
-                  id: e2.id,
+                  indikator_id: e2.id,
                   nama_indikator: e2.nama_indikator,
-                  catat_jawaban: null
+                  catat_jawaban: null,
                 });
               });
           });
         });
     },
     async postCatatan() {
+      delete this.charian.blok_lahan_id;
+      delete this.charian.tanggal;
+      this.charian.kegiatan.forEach((item) => {
+        delete item.item_pekerjaan;
+      });
+      this.charian.kegiatan.forEach((item) => {
+        item.indikator.forEach((ytem) => {
+          delete ytem.nama_indikator;
+        });
+      });
+
       try {
         let response = await this.$axios.post(
-          "api/v1/project/catat-harian",
+          `api/v1/blok/${this.$route.params.id}/catat/create`,
           this.charian
         );
         console.log(response);
@@ -279,8 +330,8 @@ export default {
         this.error = true;
         console.log(err);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
--- pages/manager-kebun/project-tanam/_id.vue
+++ pages/manager-kebun/project-tanam/_id.vue
@@ -1,74 +1,86 @@
 <template>
-  <v-row class="py-5" justify="center">
-    <v-col cols="12">
-      <!-- <pre>{{project}}</pre> -->
-      <v-card>
-        <v-card-title>
-          {{ nama_sop }}
-        </v-card-title>
-        <v-card-subtitle>
-          <div>Project Tanam ID : {{ project.id }}</div>
-        </v-card-subtitle>
-        <v-card-text>
-          <div>
-            Tahapan kebun :
-            {{
-              project.tahapan_sop_id == 1
-                ? "Persiapan Lahan dan Tanaman"
-                : project.tahapan_sop_id == 2
-                ? "Perawatan Tanaman Belum Menghasilkan"
-                : project.tahapan_sop_id == 3
-                ? "Perawatan Tanaman Menghasilkan"
-                : "Pemanenan"
-            }}
-          </div>
-          <template>
-            <span
-              >Dibuat pada {{ new Date(project.created_at).getDate() }}
-              {{ new Date(project.created_at).getMonth() | monthFormat }}
-              {{ new Date(project.created_at).getFullYear() }}
-            </span>
-          </template>
-        </v-card-text>
+  <div>
+    <v-row class="py-5" justify="center">
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>
+            {{ nama_sop }}
+          </v-card-title>
+          <v-card-subtitle>
+            <div>Project Tanam ID : {{ project.id }}</div>
+          </v-card-subtitle>
+          <v-card-text>
+            <div>
+              Tahapan kebun :
+              {{
+                project.tahapan_sop_id == 1
+                  ? "Persiapan Lahan dan Tanaman"
+                  : project.tahapan_sop_id == 2
+                  ? "Perawatan Tanaman Belum Menghasilkan"
+                  : project.tahapan_sop_id == 3
+                  ? "Perawatan Tanaman Menghasilkan"
+                  : "Pemanenan"
+              }}
+            </div>
+            <template>
+              <span
+                >Dibuat pada {{ new Date(project.created_at).getDate() }}
+                {{ new Date(project.created_at).getMonth() | monthFormat }}
+                {{ new Date(project.created_at).getFullYear() }}
+              </span>
+            </template>
+          </v-card-text>
 
-        <v-card-text>
-          <v-row>
-            <v-col
-              cols="12"
-              md="4"
-              v-for="blok in project.blok_lahan"
-              :key="blok.id"
-            >
-              <v-sheet outlined color="green" rounded>
-                <v-card
-                  outlined
-                  elevation="0"
-                  class="d-flex align-content-start flex-wrap"
-                >
-                  <v-card-title>
-                    <div class="text-subtitle-1 font-weight-bold">
-                      Blok ID {{ blok.id }}
-                    </div>
-                  </v-card-title>
-                  <v-card-text>
-                    <div class="text-body1">
-                      Luas lahan {{ blok.luas_blok }} m2
-                    </div>
-                    <div class="text-body1 ">
-                      Jumlah tanaman : {{ blok.jumlah_tanaman }}
-                    </div>
-                    <div class="text-body1 ">
-                      Umur tanaman : {{ blok.umur_tanaman }}
-                    </div>
-                  </v-card-text>
-                </v-card>
-              </v-sheet>
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+          <v-card-text>
+            <v-row>
+              <v-col
+                cols="12"
+                md="4"
+                v-for="blok in project.blok_lahan"
+                :key="blok.id"
+              >
+                <v-sheet outlined color="green" rounded>
+                  <v-card
+                    outlined
+                    elevation="0"
+                    class="d-flex align-content-start flex-wrap"
+                  >
+                    <v-card-title>
+                      <div class="text-subtitle-1 font-weight-bold">
+                        Blok ID {{ blok.id }}
+                      </div>
+                    </v-card-title>
+                    <v-card-text>
+                      <div class="text-body1">
+                        Luas lahan {{ blok.luas_blok }} m2
+                      </div>
+                      <div class="text-body1">
+                        Jumlah tanaman : {{ blok.jumlah_tanaman }}
+                      </div>
+                      <div class="text-body1">
+                        Umur tanaman : {{ blok.umur_tanaman }}
+                      </div>
+                    </v-card-text>
+                  </v-card>
+                </v-sheet>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-card tile>
+      <v-list dense>
+        <v-list-item-group color="primary">
+          <v-list-item v-for="(item, i) in pencatatan" :key="i">
+            <v-list-item-content>
+              <v-list-item-title v-text="item.catatan"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -80,19 +92,31 @@ export default {
     return {
       nama: "",
       tahapan: "",
-      project: {}
+      project: {},
+      pencatatan: [],
     };
   },
   mounted() {
     this.getProject();
+    this.getCatatanHarian();
   },
   computed: {
     nama_sop() {
       this.getSop(this.project.sop_id);
       return this.nama;
-    }
+    },
   },
   methods: {
+    async getCatatanHarian() {
+      try {
+        const res = await this.$axios.get(
+          "api/v1/blok/" + this.$route.params.id + "/catat"
+        );
+        this.pencatatan = res.data.data;
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
     async getSop(id) {
       try {
         let response = await this.$axios.get("/api/v1/sop/" + id);
@@ -108,13 +132,12 @@ export default {
           "api/v1/project/" + this.$route.params.id
         );
         this.project = response.data.data;
-        console.log(response);
       } catch (err) {
         this.error = true;
         console.log(err);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
